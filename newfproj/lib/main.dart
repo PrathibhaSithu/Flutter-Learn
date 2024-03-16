ac59d4f69133165626e87b3061e29306e99b38ba
PrathibhaSithu <sithuminiprathiba@gmail.com>
2024-03-16 15:42:05 +0530

    Update main.dart

--- newfproj/lib/main.dart
+++ newfproj/lib/main.dart
@@ -6,31 +6,32 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
- 
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 128, left: 16),
-              child: Image.asset(
-                "assets/login.png",
-                height: 250,
-              ),
-            ),
-            SizedBox(height: 20),
-            Container(
-                height: 250,
-                width: 250,
-                decoration: BoxDecoration(
-                  color: Colors.purpleAccent,
-                  borderRadius: BorderRadius.circular(10),
-                ))
-          ],
-        )
-      ),
+          backgroundColor: Colors.pinkAccent,
+          body: Column(
+            children: [
+              // Padding(
+              //   padding: const EdgeInsets.only(top: 128, left: 16),
+              //   child: Image.asset(
+              //     "assets/login.png",
+              //     height: 250,
+              //   ),
+              // ),
+              // SizedBox(height: 20),
+              Container(
+                  height: 150,
+                  width: 350,
+                  decoration: BoxDecoration(
+                    color: Colors.purple,
+                    borderRadius: BorderRadius.circular(10),
+                  ))
+            ],
+          )),
     );
   }
 }
