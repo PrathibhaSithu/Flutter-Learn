aaaac95a8f4c9ef2401b9500a3b8d17735df4bab
PrathibhaSithu <sithuminiprathiba@gmail.com>
2024-03-16 14:43:13 +0530

    Update main.dart

--- newfproj/lib/main.dart
+++ newfproj/lib/main.dart
@@ -6,12 +6,30 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
+ 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: ,
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 128, left: 16),
+              child: Image.asset(
+                "assets/login.png",
+                height: 250,
+              ),
+            ),
+            SizedBox(height: 20),
+            Container(
+                height: 250,
+                width: 250,
+                decoration: BoxDecoration(
+                  color: Colors.purpleAccent,
+                  borderRadius: BorderRadius.circular(10),
+                ))
+          ],
+        )
       ),
     );
   }
